4cc86cac0c0745bc94738cb5639d03e56158f5ec
codekissyoung <1162097842@qq.com>
2020-01-27 20:53:39 +0800

    upgrade

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,41 +1,176 @@
 package main
-import "fmt"
-type Vertex struct {
-	Lat, Long float64
+
+import (
+    "errors"
+    "fmt"
+    "time"
+)
+
+// ---------------------------------- struct -----------------------------------
+
+type user struct {
+    name string
+    age  byte
 }
 
-// 返回局部变量
-func f() *int {
-	v := 1
-	return &v
+type manager struct {
+    user
+    title string
 }
 
-// 指针入参
-func incr(p *int) int {
-	*p++
-	return *p
+type MyInt int
+
+type Printer interface {
+    Print()
 }
 
-// new 语法糖 返回指针
-func newInt() *int {
-	return new(int)
-}
+// ---------------------------------- main ------------------------------------
 
 func main() {
 
-	a:=[3]int{}
+    // test_slice()
 
-	fmt.Println(a)
+    // test_map()
 
-	v := 1
+    // test_struct()
 
-	incr( &v )
-	incr( &v )
+    // test_interface()
 
-	o := newInt()
+    // test_method()
 
-	*o = 100
 
-	fmt.Println( o )
-	fmt.Println( v )
+    /*
+    go task(1)
+
+    go task(2)
+
+    time.Sleep(time.Second * 6)
+    */
+
+    done := make(chan bool)
+    data := make(chan int)
+
+    go consumer(data, done)
+    go producer(data)
+
+    <-done
 }
+
+// ------------------------------------ func -------------------------------------
+
+// test multi return
+func div( a, b int) (int, error) {
+    if b == 0 {
+        return 0, errors.New("division by zero")
+    }
+    return a / b, nil
+}
+
+// return func type
+func test(x int) func() {
+    return func() {
+        println(x)
+    }
+}
+
+// slice
+func test_slice() {
+
+    x := make( []int, 0, 5 )
+
+    for i:= 0; i < 8; i++ {
+        x = append(x, i)
+    }
+
+    fmt.Println(x)
+}
+
+// map
+func test_map() {
+
+    m := make(map[string]int)
+
+    m["a"] = 1
+
+    fmt.Println(m)
+
+    x, ok := m["b"]
+
+    fmt.Println(x, ok)
+
+    delete(m, "a")
+
+}
+
+// struct
+func test_struct() {
+    var m manager
+
+    m.name  = "Tom"
+    m.age   = 29
+    m.title = "CTO"
+
+    // fmt.Println(m)
+    println(m.ToString())
+
+}
+
+// method
+func (x *MyInt) inc() {
+    *x++
+}
+
+func test_method() {
+
+    var x MyInt = 10
+
+    x.inc()
+
+    println(x)
+
+}
+
+func (u user) ToString() string {
+
+    return fmt.Sprintf("%+v", u)
+}
+
+func (u user) Print() {
+    fmt.Printf("%+v\n", u)
+}
+
+func test_interface () {
+    var u user;
+    u.name = "Alice"
+    u.age  = 30
+
+    var p Printer = u
+
+    p.Print()
+}
+
+func task(id int) {
+    for i :=0; i < 5; i++ {
+        fmt.Printf("%d : %d\n", id, i)
+        time.Sleep(time.Second)
+    }
+}
+
+
+func consumer( data chan int, done chan bool ) {
+
+    for x := range data {
+        println("recv:", x)
+    }
+
+    done <- true
+}
+
+func producer( data chan int ) {
+    for i := 0; i < 4; i++ {
+        data <- i
+    }
+    close(data)
+}
+
+
